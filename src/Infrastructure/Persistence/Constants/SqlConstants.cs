054dcf5ab72bc957b1cfd6c0e0067862e47889f1
Enes Aytepe <enes_aytepe@hotmail.com>
2025-02-02 20:20:45 +0300

    Added SqlContans class

---
+++ src/Infrastructure/Persistence/Constants/SqlConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace Persistence.Constants;
+
+public static class SqlConstants
+{
+    public const string CurrentUserDefault = "CURRENT_USER"; /* SqlServer, PostGres */
+
+    //public const string CurrentUTCTimeStamp = "GETUTCDATE()"; /* SqlServer */
+
+    public const string CurrentUTCTimeStamp = "timezone('utc', now())"; /* PostGres */
+}
