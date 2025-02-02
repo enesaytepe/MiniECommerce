2735145b74c68d69c1e1e45b580ab180dbc276ce
Enes Aytepe <enes_aytepe@hotmail.com>
2025-02-02 20:40:36 +0300

    AddPersistenceServices created and added on project startup

---
+++ src/Infrastructure/Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.Contexts;
+
+namespace Persistence;
+
+public static class PersistenceServiceRegistration
+{
+    public static IServiceCollection AddPersistenceServices(this IServiceCollection services, string connectionString)
+    {
+        services.AddDbContext<BaseDbContext>(
+            options => options.UseNpgsql(connectionString)
+        );
+
+        return services;
+    }
+}
--- src/Presentations/Backends/MiniECommerce.WebAPI/Program.cs
+++ src/Presentations/Backends/MiniECommerce.WebAPI/Program.cs
@@ -1,7 +1,15 @@
+using Persistence;
+
+Console.WriteLine("Web API is starting...");
 var builder = WebApplication.CreateBuilder(args);
+Console.WriteLine("IsDevelopment : " + builder.Environment.IsDevelopment());
+Console.WriteLine("IsProduction : " + builder.Environment.IsProduction());
+Console.WriteLine("Web Root Path" + builder.Environment.WebRootPath);
+Console.WriteLine("Content Root Path" + builder.Environment.ContentRootPath);
 
-
+// Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddPersistenceServices(builder.Configuration.GetConnectionString("MiniECommerceConnectionString"));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
