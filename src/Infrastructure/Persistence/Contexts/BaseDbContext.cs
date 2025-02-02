4b15e528b92d34f564b79d46290515f9cc34a58d
Enes Aytepe <enes_aytepe@hotmail.com>
2025-02-02 20:34:55 +0300

    Created db context

---
+++ src/Infrastructure/Persistence/Contexts/BaseDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Reflection;
+
+namespace Persistence.Contexts;
+
+public class BaseDbContext(DbContextOptions dbContextOptions, IConfiguration configuration) : DbContext(dbContextOptions)
+{
+    protected IConfiguration Configuration { get; set; } = configuration;
+
+    public DbSet<OperationClaim> OperationClaims { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+    public DbSet<RefreshToken> RefreshTokens { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
