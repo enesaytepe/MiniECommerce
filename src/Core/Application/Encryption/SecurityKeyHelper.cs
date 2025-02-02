096e21870fce764e95e16bb2cc0a1a912710483c
Enes Aytepe <enes_aytepe@hotmail.com>
2025-02-03 00:40:49 +0300

    Token class and methods added

---
+++ src/Core/Application/Encryption/SecurityKeyHelper.cs
@@ -0,0 +1,9 @@
+﻿using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Application.Encryption;
+
+public class SecurityKeyHelper
+{
+    public static SecurityKey CreateSecurityKey(string securityKey) => new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey));
+}
---
+++ src/Core/Application/Encryption/SigningCredentialsHelper.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.IdentityModel.Tokens;
+
+namespace Application.Encryption;
+
+public class SigningCredentialsHelper
+{
+    public static SigningCredentials CreateSigningCredentials(SecurityKey securityKey) =>
+        new(securityKey, SecurityAlgorithms.HmacSha512Signature);
+}
