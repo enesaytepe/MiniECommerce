c18d0313cae6d0ec3bfd177726ec85b318c43d98
Enes Aytepe <enes_aytepe@hotmail.com>
2025-02-03 02:40:56 +0300

    Created user service

---
+++ src/Core/Application/Services/UserService/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Entities;
+
+namespace Application.Services.UserService;
+
+public interface IUserService
+{
+    public Task<User?> GetByEmail(string email);
+    public Task<User> GetById(long id);
+    public Task<User> Update(User user);
+}
