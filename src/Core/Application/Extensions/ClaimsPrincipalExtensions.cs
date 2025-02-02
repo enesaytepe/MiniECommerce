41dd7fb0cc12b6df2e80092f099bf40dfdfea333
Enes Aytepe <enes_aytepe@hotmail.com>
2025-02-02 21:11:45 +0300

    Created new extension for claims ops

---
+++ src/Core/Application/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Claims;
+
+namespace Application.Extensions;
+
+public static class ClaimsPrincipalExtensions
+{
+    public static List<string>? Claims(this ClaimsPrincipal claimsPrincipal, string claimType) => claimsPrincipal?.FindAll(claimType)?.Select(x => x.Value).ToList();
+    public static List<string>? ClaimRoles(this ClaimsPrincipal claimsPrincipal) => claimsPrincipal?.Claims(ClaimTypes.Role);
+    public static long? GetUserId(this ClaimsPrincipal claimsPrincipal) => claimsPrincipal?.Claims(ClaimTypes.NameIdentifier)?.Select(long.Parse).FirstOrDefault();
+}
