6f66e77c5e0cc5d0fc0014de230800bc4e800754
Enes Aytepe <enes_aytepe@hotmail.com>
2025-02-03 11:16:10 +0300

    Some props are changed to nullable props on RefreshToken entity model

--- src/Core/Domain/Entities/RefreshToken.cs
+++ src/Core/Domain/Entities/RefreshToken.cs
@@ -8,10 +8,10 @@ public class RefreshToken : Entity<Guid>
     public DateTime Created { get; set; }
     public string CreatedByIp { get; set; }
     public DateTime? Revoked { get; set; }
-    public string RevokedByIp { get; set; }
-    public string ReplacedByToken { get; set; }
+    public string? RevokedByIp { get; set; }
+    public string? ReplacedByToken { get; set; }
 
-    public string ReasonRevoked { get; set; }
+    public string? ReasonRevoked { get; set; }
 
     //public bool IsExpired => DateTime.UtcNow >= Expires;
     //public bool IsRevoked => Revoked != null;
--- src/Infrastructure/Persistence/Migrations/20250203003051_RefreshToken_Modified2.Designer.cs
+++ src/Infrastructure/Persistence/Migrations/20250203003051_RefreshToken_Modified2.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Persistence.Contexts;
@@ -11,9 +12,11 @@ using Persistence.Contexts;
 namespace Persistence.Migrations
 {
     [DbContext(typeof(BaseDbContext))]
-    partial class BaseDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250203003051_RefreshToken_Modified2")]
+    partial class RefreshToken_Modified2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -86,6 +89,7 @@ namespace Persistence.Migrations
                         .HasColumnType("timestamp with time zone");
 
                     b.Property<string>("ReasonRevoked")
+                        .IsRequired()
                         .HasColumnType("text");
 
                     b.Property<string>("ReplacedByToken")
